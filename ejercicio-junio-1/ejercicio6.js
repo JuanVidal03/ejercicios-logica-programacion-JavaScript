4149e1ef32749881d4e6643b4b996027d5b85d74
Juan Vidal <juanmvg2003@gmail.com>
2024-06-04 10:36:23 -0500

    ejercicio-junio-1 ejercicio 6

---
+++ ejercicio-junio-1/ejercicio6.js
@@ -0,0 +1,8 @@
+// Imprimir números del 1 al 10:
+// Escribe un algoritmo que use un ciclo while para imprimir los números del 1 al 10.
+
+
+// imprimiendo numeros del 1 al 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
